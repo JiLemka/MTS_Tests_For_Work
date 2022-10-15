1d4c2cd776e96133155829aa0f12de6e1d52b2c0
JiLemka <antonmefedov@icloud.com>
2022-10-15 19:31:42 +0300

    Initial commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    /*
+    static void Main(string[] args)
+    {
+        try
+        {
+            FailProcess();
+        }
+        catch
+        {
+        }
+
+        Console.WriteLine("Failed to fail process!");
+        Console.ReadKey();
+    }
+
+    static void FailProcess()
+    {
+        NullException(5, 0);
+    }
+
+    static int NullException(int a, int b)
+    {
+        int Exception = a / b;
+
+        return Exception;
+    }
+    */
+}
