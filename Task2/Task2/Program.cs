fe675553c0b49c41319043ad8b8677179c2085ce
JiLemka <antonmefedov@icloud.com>
2022-10-17 22:49:58 +0300

    Complete Task 2

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Globalization;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static readonly IFormatProvider _ifp = CultureInfo.InvariantCulture;
+
+    class Number
+    {
+        readonly int _number;
+
+        public Number(int number)
+        {
+            _number = number;
+        }
+
+        public override string ToString()
+        {
+            return _number.ToString(_ifp);
+        }
+        
+        public static string operator +(Number someValue1, string someValue2)
+        {
+            return (someValue1._number + int.Parse(someValue2)).ToString();
+        }
+
+    }
+    
+    static void Main(string[] args)
+    {
+        int someValue1 = 10;
+        int someValue2 = 5;
+
+        string result = new Number(someValue1) + someValue2.ToString(_ifp);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
