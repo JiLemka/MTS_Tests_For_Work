872a5d53ffd65424031f975b949f8343bdcb7e9d
JiLemka <antonmefedov@icloud.com>
2022-10-18 22:37:50 +0300

    Complete Task 5

--- Task5/Task5/Program.cs
+++ Task5/Task5/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.IO;
 
-Console.WriteLine("Hello, World!");
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransformToElephant();
+            //Можно просто закомментировать строку Console.WriteLine("Муха") :)
+            Console.WriteLine("Муха");
+            //... custom application code
+            
+        }
+
+        static void TransformToElephant()
+        {
+            Console.WriteLine("Слон");
+            Console.SetOut(new MyWriter());
+        }
+    }
+
+    class MyWriter : StringWriter
+    {
+        private TextWriter skipOne = Console.Out;
+
+        public override void WriteLine(string? value)
+        {
+            Console.SetOut(skipOne);
+        }
+    }
+}
