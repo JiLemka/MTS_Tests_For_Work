efb83b3d34cc4e37b5132ca785694e0d55e64025
JiLemka <antonmefedov@icloud.com>
2022-10-19 19:41:59 +0300

    Finish commit

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections;
 
-Console.WriteLine("Hello, World!");
+namespace Task3
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new[] { 1, 2, 3, 4 }.EnumerateFromTail(2);
+            foreach (var item in result)
+                Console.WriteLine($"{item} ");
+        }
+        
+        public static IEnumerable<(T item, int? tail)> EnumerateFromTail<T>(this IEnumerable<T> enumerable,
+            int? tailLength)
+        {
+            var array = enumerable.ToArray();
+            List<(T item, int? tail)> result = new List<(T item, int? tail)>();
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < tailLength)
+                    result.Add((array[array.Length - i - 1], count++));
+                else
+                    result.Add((array[array.Length - i - 1], null));
+            }
+            
+            return result;
+        }
+    }
+}
--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+namespace Task4
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rnd = new int[200];
+            IEnumerable<int> result = new int[200];
+            Random rand = new Random();
+
+            for (int x = 0; x < 200; x++)
+            {
+                rnd[x] = rand.Next(100, 2001);
+                Console.Write($"{rnd[x]} ");
+            }
+
+            result = Sort(rnd, 600, 2000);
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in result)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        /// <summary>
+        /// Возвращает отсортированный по возрастанию поток чисел
+        /// </summary>
+        /// <param name="inputStream">Поток чисел от 0 до maxValue. Длина потока не превышает миллиарда чисел.</param>
+        /// <param name="sortFactor">Фактор упорядоченности потока. Неотрицательное число. Если в потоке встретилось число x, то в нём больше не встретятся числа меньше, чем (x - sortFactor).</param>
+        /// <param name="maxValue">Максимально возможное значение чисел в потоке. Неотрицательное число, не превышающее 2000.</param>
+        /// <returns>Отсортированный по возрастанию поток чисел.</returns>
+        static IEnumerable<int> Sort(IEnumerable<int> inputStream, int sortFactor, int maxValue)
+        {
+            int[] values = new int[maxValue + 1];
+            int currentValue = 0;
+
+            foreach (int x in inputStream)
+            {
+                ++values[x];
+
+                int minPossible = x - sortFactor;
+                while (currentValue < minPossible)
+                {
+                    while (--values[currentValue] >= 0)
+                        yield return currentValue;
+                    currentValue++;
+                }
+            }
+
+            while (currentValue < values.Length)
+            {
+                while (--values[currentValue] >= 0)
+                    yield return currentValue;
+                currentValue++;
+            }
+        }
+    }
+}
